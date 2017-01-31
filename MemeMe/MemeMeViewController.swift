37aad81dffb13c7f24d231483ddca8547e41397f
Li, Haibo <haibol@amazon.com>
2017-01-31 11:36:33 +0800

    Init

---
+++ MemeMe/MemeMeViewController.swift
@@ -0,0 +1,56 @@
+//
+//  MemeMeViewController.swift
+//  MemeMe
+//
+//  Created by Li, Haibo on 1/30/17.
+//  Copyright © 2017 Amazon. All rights reserved.
+//
+
+import UIKit
+
+let memeTextAttributes:[String:Any] = [
+    NSStrokeColorAttributeName: UIColor.white,
+    NSForegroundColorAttributeName: UIColor.white,
+    NSFontAttributeName: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+    NSStrokeWidthAttributeName: 1.0]
+
+class MemeMeViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    @IBOutlet weak var imagePickerView: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        setDefaultUI()
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+    }
+
+    @IBAction func pickAnImageFromAlbum(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+
+    @IBAction func pickAnImageFromCamera(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func setDefaultUI() {
+        topTextField.defaultTextAttributes = memeTextAttributes
+        bottomTextField.defaultTextAttributes = memeTextAttributes
+        topTextField.text = "TOP"
+        bottomTextField.text = "BOTTOM"
+    }
+}
